8a69d798f4de799c0a163ca1fbcae165e7bba81a
Fernando Campos <fernando.campos@ucdenver.edu>
2021-03-09 15:12:49 -0700

    adding correct author display code, changes to undefined displays, and visual style to previous and next buttons

--- components/bookDisplay.js
+++ components/bookDisplay.js
@@ -9,9 +9,9 @@ app.component('book-display', {
     template:
     /*html*/
     `<ul class="col-sm-12 col-md-6 col-xl-4">
-    <a class="list-group-item list-group-item-action active" href="this.bookObj.selfLink">{{this.bookObj.volumeInfo.title}}</a>
-    <li class="list-group-item">{{this.bookObj.volumeInfo.authors}}</li>
-    <li class="list-group-item">{{this.bookObj.volumeInfo.publisher}}, {{this.bookObj.volumeInfo.publishedDate}}</li>
+    <a class="list-group-item list-group-item-action active" href="this.bookObj.selfLink" v-show = "bookObj.volumeInfo.title != undefined">{{this.bookObj.volumeInfo.title}}</a>
+    <li class="list-group-item" v-show = "bookObj.volumeInfo.authors != undefined"><li class="list-group-item" v-for="author in bookObj.volumeInfo.authors">{{author}}</li></li>
+    <li class="list-group-item" v-show = "bookObj.volumeInfo.publisher != undefined && bookObj.volumeInfo.publishedDate != undefined">{{this.bookObj.volumeInfo.publisher}}, {{this.bookObj.volumeInfo.publishedDate}}</li>
     </ul>`,
     computed:{
         bookObj(){
