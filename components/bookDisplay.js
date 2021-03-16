961d5c0a6b77d3e31db0f379cbb8df967b313513
Fernando Campos <fernando.campos@ucdenver.edu>
2021-03-15 21:23:33 -0600

    fixed small error where clicking on book title would not navigate to link

--- components/bookDisplay.js
+++ components/bookDisplay.js
@@ -9,7 +9,7 @@ app.component('book-display', {
     template:
     /*html*/
     `<ul class="col-sm-12 col-md-6 col-xl-4">
-    <a class="list-group-item list-group-item-action active" href="this.bookObj.selfLink" v-show = "bookObj.volumeInfo.title != undefined">{{this.bookObj.volumeInfo.title}}</a>
+    <a class="list-group-item list-group-item-action active" :href="this.bookObj.selfLink" v-show = "bookObj.volumeInfo.title != undefined">{{this.bookObj.volumeInfo.title}}</a>
     <li class="list-group-item" v-if = "bookObj.volumeInfo.imageLinks != undefined"><img :src=bookObj.volumeInfo.imageLinks.thumbnail></li>
     <li class="list-group-item" v-show = "bookObj.volumeInfo.authors != undefined"><li class="list-group-item" v-for="author in bookObj.volumeInfo.authors">{{author}}</li></li>
     <li class="list-group-item" v-show = "bookObj.volumeInfo.publisher != undefined && bookObj.volumeInfo.publishedDate != undefined">{{this.bookObj.volumeInfo.publisher}}, {{this.bookObj.volumeInfo.publishedDate}}</li>
